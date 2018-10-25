6c3cb62fa84a3c16f410ba1bf1576ea323f7e705
Adam Lancaster <arglanc@users.noreply.github.com>
2018-10-25 16:47:54 +0100

    Scaffold Identity using new recommended method

---
+++ Gatekeeper/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gatekeeper.Areas.Identity.Data;
+using Gatekeeper.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Gatekeeper.Areas.Identity.IdentityHostingStartup))]
+namespace Gatekeeper.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<GatekeeperContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("GatekeeperContextConnection")));
+
+                services.AddDefaultIdentity<GatekeeperUser>()
+                    .AddEntityFrameworkStores<GatekeeperContext>();
+            });
+        }
+    }
+}
