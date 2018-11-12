c559d2579011fe2df4996595332c6d6d85bd1b5f
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-12 14:20:10 +0000

    Update Gatekeeper/Controllers/StatusController.cs
    Add most basic implementation of /Status endpoint

---
+++ Gatekeeper/Controllers/StatusController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gatekeeper.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        // GET: Status
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
