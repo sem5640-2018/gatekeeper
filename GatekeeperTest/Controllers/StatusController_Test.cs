e0d0fb0d75768f6626f723ebc54f83a48da5fd2a
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-12 14:38:10 +0000

    Update GatekeeperTest/Controllers/StatusController_Test.cs
    Add tests for /Status endpoint

---
+++ GatekeeperTest/Controllers/StatusController_Test.cs
@@ -0,0 +1,29 @@
+﻿using Gatekeeper.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GatekeeperTest.Controllers
+{
+    public class StatusController_Test
+    {
+        [Fact]
+        public void Get_ReturnsOk()
+        {
+            var controller = new StatusController();
+            var result = controller.Get();
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void Get_HasCorrectContent()
+        {
+            var controller = new StatusController();
+            var result = controller.Get();
+            var content = result as OkObjectResult;
+            Assert.Null(content);
+        }
+    }
+}
