737ca844555bf8fb8bf642f08913c13865a5efd7
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-15 16:19:12 +0000

    Update Startup.cs
    add token authentication for protecting our own apis

--- Gatekeeper/Startup.cs
+++ Gatekeeper/Startup.cs
@@ -80,6 +80,12 @@ namespace Gatekeeper
                 })
                 .AddAspNetIdentity<GatekeeperUser>();
 
+            services.AddAuthentication().AddIdentityServerAuthentication("token", options =>
+            {
+                options.Authority =GatekeeperConfig.GetValue<string>("BaseUrl");
+                options.ApiName = "gatekeeper";
+            });
+
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("Administrator", pb => pb.RequireClaim("user_type", "administrator"));
