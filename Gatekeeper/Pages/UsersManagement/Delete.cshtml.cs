64186577cce1b69a64983d3f8a487061e03777d4
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-16 15:23:25 +0000

    Replace Index/Delete with razor pages

---
+++ Gatekeeper/Pages/UsersManagement/Delete.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gatekeeper.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gatekeeper.Pages.UsersManagement
+{
+    [Authorize("Administrator")]
+    public class DeleteModel : PageModel
+    {
+        private readonly UserManager<GatekeeperUser> _userManager;
+
+        public DeleteModel(UserManager<GatekeeperUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public GatekeeperUser UserToDelete { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            UserToDelete = user;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _userManager.DeleteAsync(user);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ Gatekeeper/Pages/UsersManagement/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gatekeeper.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gatekeeper.Pages.UsersManagement
+{
+    [Authorize("Administrator")]
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<GatekeeperUser> _userManager;
+
+        public IndexModel(UserManager<GatekeeperUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public List<GatekeeperUser> Users { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            Users = await _userManager.Users.ToListAsync();
+
+            return Page();
+        }
+    }
+}
