0aa95d42aeb9d468483761e3d8d720f3de3d268c
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-16 15:49:54 +0000

    Update Index.cshtml.cs
    Redirect to login instead of error

--- Gatekeeper/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Gatekeeper/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -54,7 +54,8 @@ namespace Gatekeeper.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return RedirectToPage("../Login");
+                //return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
             var userName = await _userManager.GetUserNameAsync(user);
