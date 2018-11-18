c51a5f9873c5d6c43148c170d15dfad33e8ce231
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-18 16:21:18 +0000

    cant run dotnet ef from runtime

--- Gatekeeper/scripts/start.sh
+++
@@ -1,4 +0,0 @@
-dotnet ef database update --context GatekeeperContext
-dotnet ef database update --context ConfigurationDbContext
-dotnet ef database update --context PersistedGrantDbContext
-dotnet Gatekeeper.dll
