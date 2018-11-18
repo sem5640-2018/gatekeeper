0b777bef428c2cccd5f26d6c653bb4fca6bf3ae3
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-18 15:30:06 +0000

    use start script

---
+++ bin/start.sh
@@ -0,0 +1,4 @@
+dotnet ef database update --context GatekeeperContext
+dotnet ef database update --context ConfigurationDbContext
+dotnet ef database update --context PersistedGrantDbContext
+dotnet Gatekeeper.dll
